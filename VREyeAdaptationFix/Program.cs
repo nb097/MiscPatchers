c024053488878df9c213c9e4ca41b2042ede8515
CritLoren <madalin.vlad.design@gmail.com>
2024-01-19 18:30:04 +0000

    Initial release of VREyeAdaptionFix

--- VREyeAdaptationFix/Program.cs
+++ VREyeAdaptationFix/Program.cs
@@ -1,6 +1,6 @@
 using Mutagen.Bethesda;
-using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
 
 namespace VREyeAdaptationFix
 {
@@ -8,15 +8,30 @@ namespace VREyeAdaptationFix
     {
         public static async Task<int> Main(string[] args)
         {
-            return await SynthesisPipeline.Instance
-                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+            return await SynthesisPipeline
+                .Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
                 .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
                 .Run(args);
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            foreach (
+                var imagespace in state.LoadOrder.PriorityOrder.ImageSpace().WinningOverrides()
+            )
+            {
+                if (imagespace.Cinematic == null || imagespace.Cinematic.Contrast == 1f)
+                    continue;
+
+                var moddedImagespace = imagespace.DeepCopy();
+
+                if (moddedImagespace.Cinematic == null || moddedImagespace.Cinematic.Contrast == 1f)
+                    continue;
+
+                moddedImagespace.Cinematic.Contrast = 1f;
+                state.PatchMod.ImageSpaces.Set(moddedImagespace);
+                Console.WriteLine($"Patched {moddedImagespace.EditorID}");
+            }
         }
     }
 }
