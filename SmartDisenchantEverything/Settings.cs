c1d3cc165cb727af241f0fd6e3bfe97d77d830a0
CritLoren <madalin.vlad.design@gmail.com>
2024-02-03 12:46:48 +0000

    Release of Smart Disenchant Everything

---
+++ SmartDisenchantEverything/Settings.cs
@@ -0,0 +1,23 @@
+using Mutagen.Bethesda.Synthesis.Settings;
+
+namespace SmartDisenchantEverything
+{
+    public class Settings
+    {
+        [SynthesisSettingName(
+            "Patch items with effects that require specific equipment (some enchantments look for item sets)"
+        )]
+        [SynthesisDescription(
+            "Some enchantments might look for non-item keywords, like checking if your equipment is comprised of light armour"
+        )]
+        public bool PatchEffectCond { get; set; } = false;
+
+        [SynthesisSettingName(
+            "Patch items with non-quest script properties (some items have spells attached via scripts instead of enchantments)"
+        )]
+        [SynthesisDescription(
+            "Sometimes items have the enchantment effect attached as a script, sometimes it's just a way to push a quest forward"
+        )]
+        public bool PatchScriptVDAM { get; set; } = false;
+    }
+}
